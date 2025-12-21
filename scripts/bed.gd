66751b41d5d4932842cea2a864c70dfd7beb0142
AzeOcram <mrodon@up.edu.ph>
2025-12-22 02:20:00 +0800

    feat: added monster creeping simulation

--- scripts/bed.gd
+++ scripts/bed.gd
@@ -1,32 +1,85 @@
 extends Node2D
 
+# Track monsters in each area
+var monsters_in_blanket = []
+var monsters_in_right = []
+var monsters_in_left = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$BlanketArea.body_entered.connect(_on_blanket_body_entered)
+	$BlanketArea.body_exited.connect(_on_blanket_body_exited)
+	
+	$RightArea.body_entered.connect(_on_right_body_entered)
+	$RightArea.body_exited.connect(_on_right_body_exited)
+	
+	$LeftArea.body_entered.connect(_on_left_body_entered)
+	$LeftArea.body_exited.connect(_on_left_body_exited)
 
+func _on_blanket_body_entered(body):
+	if body.is_in_group("monsters"):
+		monsters_in_blanket.append(body)
+		print(body.name + " ADDED to blanket array")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_blanket_body_exited(body):
+	if body.is_in_group("monsters"):
+		monsters_in_blanket.erase(body)
 
+func _on_right_body_entered(body):
+	if body.is_in_group("monsters"):
+		monsters_in_right.append(body)
+		print(body.name + " entered right")
+
+func _on_right_body_exited(body):
+	if body.is_in_group("monsters"):
+		monsters_in_right.erase(body)
+
+func _on_left_body_entered(body):
+	if body.is_in_group("monsters"):
+		monsters_in_left.append(body)
+		print(body.name + " entered left")
+
+func _on_left_body_exited(body):
+	if body.is_in_group("monsters"):
+		monsters_in_left.erase(body)
+
+# When clicking areas, reset monsters in that area
 func _on_blanket_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Blanket clicked!")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("=== BLANKET CLICKED ===")
+		print("Monsters in blanket array: ", monsters_in_blanket.size())
+		for m in monsters_in_blanket:
+			print("  - ", m.name)
+		print("Monsters in right array: ", monsters_in_right.size())
+		for m in monsters_in_right:
+			print("  - ", m.name)
+		print("Monsters in left array: ", monsters_in_left.size())
+		for m in monsters_in_left:
+			print("  - ", m.name)
+		
+		for monster in monsters_in_blanket:
+			monster.reset_state()
 
 func _on_right_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Right clicked!")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("=== RIGHT CLICKED ===")
+		print("Monsters in right array: ", monsters_in_right.size())
+		for m in monsters_in_right:
+			print("  - ", m.name)
+		
+		for monster in monsters_in_right:
+			monster.reset_state()
 
 func _on_left_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Left clicked!")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("Clicked Left - Resetting monsters")
+		for monster in monsters_in_left:
+			monster.reset_state()
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == Key.KEY_Q:
 			print("Q pressed")
-		if event.keycode == Key.KEY_W:
+		elif event.keycode == Key.KEY_W:
 			print("W pressed")
-		if event.keycode == Key.KEY_E:
+		elif event.keycode == Key.KEY_E:
 			print("E pressed")
--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -6,6 +6,21 @@ extends Node2D
 func _ready() -> void:
 	ui.startProgressBar(3.0)
 	ui.startTimer(100)
+	
+	# DEBUG: Check if bed and monsters exist
+	await get_tree().create_timer(0.5).timeout
+	print("=== SCENE CHECK ===")
+	print("Bed exists: ", has_node("Bed"))
+	print("Monsters exists: ", has_node("Monsters"))
+	if has_node("Monsters"):
+		print("Monster1 exists: ", $Monsters.has_node("Monster1"))
+	
+	# Check positions
+	print("=== POSITIONS ===")
+	print("Bed position: ", $Bed.global_position)
+	print("Monster1 position: ", $Monsters/Monster1.global_position)
+	print("Monster2 position: ", $Monsters/Monster2.global_position)
+	print("Monster3 position: ", $Monsters/Monster3.global_position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
